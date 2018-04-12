abefdad9ab2086aae667697e4f826546e5642668
FlyHighXX <diacouthman@gmail.com>
2018-04-12 18:45:23 +0200

    HillCipher.java edits

--- Hill Cipher Assignment/hillcipher.java
+++ Hill Cipher Assignment/hillcipher.java
@@ -1,7 +1,13 @@
 public class HillCipher{
   public static void main(String[] args) {
-      int radix = Integer.parseInt(args[0]);
-      int block_size = Integer.parseInt(args[1]);
+      int radix=0;
+      int block_size=0;
+      try {
+        radix = Integer.parseInt(args[0]);
+        block_size = Integer.parseInt(args[1]);
+      } catch(NumberFormatException e) {
+
+      }
 
       System.out.println("radix: " + radix);
       System.out.println("block size:" + block_size);
