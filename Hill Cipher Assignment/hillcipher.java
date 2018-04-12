5556c0c4c26afeeb986081adbb829980bdd6bc3d
FlyHighXX <diacouthman@gmail.com>
2018-04-12 18:55:22 +0200

    v

--- Hill Cipher Assignment/hillcipher.java
+++ Hill Cipher Assignment/hillcipher.java
@@ -1,12 +1,12 @@
 public class HillCipher{
-  public static void main(String[] args) throws NumberFormatException {
+  public static void main(String[] args) {
       int radix=0;
       int block_size=0;
       try {
         radix = Integer.parseInt(args[0]);
         block_size = Integer.parseInt(args[1]);
       } catch(NumberFormatException e) {
-        throw new NumberFormatException("You have entered a wrong input value. Try again");
+        System.out.println("You have entered the wrong data for radix or block_size");
       }
 
       System.out.println("radix: " + radix);
