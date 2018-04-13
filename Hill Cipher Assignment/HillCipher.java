858b8a4c0a6572bc525b749d0ebf08846bd38903
FlyHighXX <diacouthman@gmail.com>
2018-04-13 16:42:42 +0200

    v

--- Hill Cipher Assignment/HillCipher.java
+++ Hill Cipher Assignment/HillCipher.java
@@ -110,7 +110,7 @@ public class HillCipher{
         Vector<ModuloInteger> vector = DenseVector.valueOf(vector);
         Vector<ModuloInteger> encVector = DenseVector.valueOf(key.times(vector));
         for(int k=0; k<v.length; k++){
-          resultList.add(encVector.get(k).moduloValue());
+          resultList.add((Integer)encVector.get(k).moduloValue().longValue());
         }
       }
       return resultList;
