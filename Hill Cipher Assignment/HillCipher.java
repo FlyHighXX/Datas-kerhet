081cbe7629efabc5cf3403c41f01ffc39b62104c
FlyHighXX <diacouthman@gmail.com>
2018-04-13 16:36:46 +0200

    v

--- Hill Cipher Assignment/HillCipher.java
+++ Hill Cipher Assignment/HillCipher.java
@@ -8,7 +8,7 @@ import org.jscience.mathematics.vector.Vector;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
-import java.io.PrintWriter
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
