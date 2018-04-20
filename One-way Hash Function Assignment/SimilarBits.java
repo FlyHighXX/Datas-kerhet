0e2877e94209e2888a1e4beb94dc5f42f71fe541
FlyHighXX <diacouthman@gmail.com>
2018-04-20 17:56:41 +0200

    v

--- One-way Hash Function Assignment/SimilarBits.java
+++ One-way Hash Function Assignment/SimilarBits.java
@@ -1,7 +1,7 @@
 public class SimilarBits {
     public static void main(String[] args) {
-        int h1 = Integer.parseInt(args[0]);
-        int h2 = Integer.parseInt(args[1]);
+        int h1 = Integer.parseInt(args[0],2);
+        int h2 = Integer.parseInt(args[1],2);
         System.out.println(h1);
         System.out.println(h2);
     }
