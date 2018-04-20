8cc27356209f28049584785bc13ee8d1e45c1495
FlyHighXX <diacouthman@gmail.com>
2018-04-20 18:29:30 +0200

    v

--- One-way Hash Function Assignment/SimilarBits.java
+++ One-way Hash Function Assignment/SimilarBits.java
@@ -1,13 +1,13 @@
 public class SimilarBits {
     public static void main(String[] args) {
-        long h1  =c15c01d3365b51f2;
-        long h12 =c5ec171f5fb91a1b;
-        long h13 =b01c19ea33cf55ee;
-        long h14 =618f36d869381588;
-        long h2  =7ef8f221fa560414;
-        long h22 =7f8daa5ace40306f;
-        long h23 =c60ffb1cdb118bcf;
-        long h24 =d98165bac9a4d028;
+        long h1  =0xc15c01d3365b51f2;
+        long h12 =0xc5ec171f5fb91a1b;
+        long h13 =0xb01c19ea33cf55ee;
+        long h14 =0x618f36d869381588;
+        long h2  =0x7ef8f221fa560414;
+        long h22 =0x7f8daa5ace40306f;
+        long h23 =0xc60ffb1cdb118bcf;
+        long h24 =0xd98165bac9a4d028;
         String h1b = Integer.toBinaryString(h1)+Integer.toBinaryString(h12)+Integer.toBinaryString(h13)+Integer.toBinaryString(h14);
         String h2b = Integer.toBinaryString(h2)+Integer.toBinaryString(h22)+Integer.toBinaryString(h23)+Integer.toBinaryString(h24);
         int length;
